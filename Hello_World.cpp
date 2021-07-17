9d5257e498c0301cc4196a2da1dbb6c5a2788a5c
VaibhavSi47 <imvaibhav777@gmail.com>
2021-07-17 11:20:38 +0530

    Update Hello_World.cpp

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -6,6 +6,6 @@ int main()
   char name[30];
   printf("Enter your name :");
   scanf("%s",&name);
-  printf("\nHello %s \nWelcome To my Programming World", name);
+  printf("\nHello %s! \nWelcome To my Programming World", name);
   return 0;
 }
