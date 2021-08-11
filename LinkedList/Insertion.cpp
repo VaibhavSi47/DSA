73ab702b0bfaf6fc0294b8e37b2a0bcd5be7a7ae
VaibhavSi47 <imvaibhav777@gmail.com>
2021-08-11 10:18:55 +0530

    Create Insertion.cpp

---
+++ LinkedList/Insertion.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+  int data;
+  struct node* next;
+};
+
+void Start(struct node*){
+  struct node* new_node = (struct node*)malloc(sizeof(new_node));
+  new_node->data = data
+}
+
+int main(){
+  int data, choice;
+  struct node* head = NULL;
+  printf("******************************************");
+  printf("\nWelcome to ListedList Insertion Program.");
+  printf("\nEnter the data: ");
+  printf("\nPlease select whether do you want to:\n1.Create new list.\n2.Insert at the start.\n3.Insert in the middle.\n4.Insert at the end.\n5.Display the list.");
+  scanf("%d",&choice);
+  switch (choice)
+  {
+  case 1:
+    printf("\nCreating new List");
+    
+
+    break;
+  
+  default:
+    break;
+  }
+  void Start(head);
+  void in_btw(head);
+  void end(head);
+}
